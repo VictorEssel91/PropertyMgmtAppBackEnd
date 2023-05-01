83798e7df16faee0cf7bec553d3cbb7d007d014d
unknown <victoressel91@gmail.com>
2023-04-30 21:01:24 -0400

    add Repo files and update PropertyService files. Update application.properties file in resources

---
+++ PropertyMgmtApi/src/main/java/com/victor/SpringProject/Service/PropertyService.java
@@ -0,0 +1,7 @@
+package com.victor.SpringProject.Service;
+
+import com.victor.SpringProject.DTO.PropertySaveDTO;
+
+public interface PropertyService {
+    String addProperty(PropertySaveDTO propertySaveDTO);
+}
